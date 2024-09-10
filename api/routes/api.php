adc48d5858a81f9bf7edd7df6fb623c938dc50a0
serenitydevina <147176974+serenitydevina@users.noreply.github.com>
2024-09-10 15:11:43 +0700

    api ( 10 September 2024)

---
+++ api/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\ProdiController;
+use App\Models\Fakultas;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/fakultas',[FakultasController::class,'index']);
+Route::get('/prodi',[ProdiController::class,'index']);
