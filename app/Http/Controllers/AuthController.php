00c50e8dd2b41ef8139681670b89234a4726c5b2
serenitydevina <147176974+serenitydevina@users.noreply.github.com>
2024-09-24 18:55:11 +0700

    pertemuan_5 (24 September 2024)

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+        public function register(Request $request)
+{
+    $validate = $request->validate([
+        'name'  => 'required',
+        'email' => 'required|email',
+        'password'  => 'required',
+        'password_confirmation' => 'required|same:password',
+    ]);
+
+    $validate['password'] = bcrypt($request->password);
+
+    $user = User::create($validate);
+    $success['token'] = $user->createToken('MDPApp')->plainTextToken;
+    $success['name'] = $user->name;
+
+    return response()->json($success, Response::HTTP_CREATED);
+}
+
+public function login(Request $request)
+{
+    if(\Auth::attempt([
+        'email' => $request->email,
+        'password' => $request->password
+    ])) {
+        $user = \Auth::user();
+        $success['token'] = $user->createToken('MDPApp')->plainTextToken;
+        $success['name'] = $user->name;
+        return response()->json($success, 201);
+    } else {
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+}
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\ProdiController;
-use App\Models\Fakultas;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,7 +11,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/fakultas',[FakultasController::class,'index']);
+Route::middleware('auth:sanctum')->get('/fakultas', [FakultasController::class, 'index']);
+// Route::get('/fakultas',[FakultasController::class,'index']);
 Route::get('/prodi',[ProdiController::class,'index']);
 Route::post('/fakultas',[FakultasController::class,'store']);
 Route::post('/prodi',[ProdiController::class,'store']);
@@ -21,3 +22,5 @@ Route::patch('/fakultas/{fakultas}',[FakultasController::class,'update']);
 Route::patch('/prodi/{prodi}',[ProdiController::class,'update']);
 Route::delete('/fakultas/{fakultas}',[FakultasController::class,'destroy']);
 Route::delete('/prodi/{prodi}',[ProdiController::class,'destroy']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
